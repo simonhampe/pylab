d845c1a2dd88e69a989c8d772072fea17d004c1b
hampe <simon.hampe@googlemail.com>
2016-12-25 15:32:24 +0100

    Adding scaling method for Labyrinth

--- Labyrinth.py
+++ Labyrinth.py
@@ -9,4 +9,17 @@ class Labyrinth :
         self.end = end
         self.data = data
 
+    def scale(self, factor) :
+        self.width = int(self.width * factor)
+        self.height = int(self.height * factor)
+        self.start = tuple( [int(x * factor) for x in self.start])
+        self.end = tuple( [int(x * factor) for x in self.end])
+        new_data = {}
+        for i in range(0,self.width) :
+            for j in range(0,self.height) :
+                scaled_point = (int(i/factor), int(j/factor))
+                if scaled_point in self.data.keys() :
+                    new_data[(i,j)] = self.data[scaled_point]
+        self.data = new_data
+
 
