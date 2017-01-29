e24c83f3c7f01f9194c43c9e0dba07130996aedd
hampe <simon.hampe@googlemail.com>
2017-01-29 18:30:26 +0100

    Working on Dungeon generator

--- DungeonLabGenerator.py
+++ DungeonLabGenerator.py
@@ -1,7 +1,14 @@
-import LabyrinthConstants
+from LabyrinthConstants import LAB_WALL, LAB_FLOOR
 import random
 from pygame import Rect
+from GridMatrix import GridMatrix
 from Labyrinth import Labyrinth
+
+class DungeonData :
+
+    def __init__(self, data, valid) :
+        self.data = data
+        self.valid = valid
 
 """
 Contains the definition of a Labyrinth generator, which generates dungeon-style labyrinths
@@ -17,28 +24,35 @@ class DungeonLabGenerator :
         self.min_room_dims = (int(width/10),int(height/10))
         self.max_room_dims = (int(width/5),int(width/5))
         avg_room_size = (self.min_room_dims[0] + self.max_room_dims[0])*(self.min_room_dims[1] + self.max_room_dims[1]) * 1/4
-        self.room_tries = int(width * height / (avg_room_size)) * 50
+        self.room_tries = int(width * height / (avg_room_size)) * 20 
         self.boundary_buffer = 1
         self.room_boundary_buffer = 1
 
-    def _fill_rect(self, rc) :
-        r_dict = {}
-        for i in range(rc.left + self.room_boundary_buffer, rc.right - self.room_boundary_buffer) :
-            for j in range(rc.top + self.room_boundary_buffer, rc.bottom - self.room_boundary_buffer) :
-                r_dict[(i,j)] = LabyrinthConstants.LAB_FLOOR
-        return r_dict
+    def _count_alive_neighbours(self,node) :
+        return len([x for x in node.neighbours() if x.data.data == LAB_FLOOR])
 
-    def _is_valid_point(self, corridor_dict, lab_dict) :
-        
+    def _is_valid(self,node) :
+        return self._count_alive_neighbours(node) == 0
 
-    def _fill_corridors(self, lab_dict) :
-        valid_points = [ (a,b) for a in range(1,self.width-2) for b in range(1,self.height-2) if (a,b) not in lab_dict.keys()]
-        starting_point = random.choice(valid_points)
-        valid_points.remove(starting_point)
-        while len(valid_points) > 0 :
-
-
-
+    def _build_corridors(self,GM) :
+        while True :
+            #Find valid point
+            current_point = None
+            for node in GM.rowwise_iterator() :
+                if node.data.valid :
+                    current_point = node
+                    break
+            if not current_point :
+                return
+            while current_point :
+                candidates = [x for x in current_point.straight_neighbours() if x.data.valid]
+                for nb in current_point.straight_neighbours() :
+                    nb.data.valid = False
+                
+                current_point.data = DungeonData(LAB_FLOOR, False)
+                #Pick a random neighbour (or stop if there is no valid neighbour)
+                current_point = None if len(candidates) == 0 else random.choice(candidates)
+            
 
     def generate_labyrinth(self) :
         roomlist = []
@@ -50,14 +64,27 @@ class DungeonLabGenerator :
             nextroom = Rect(rlft, rtop, rwdt, rhgt)
             if nextroom.collidelist(roomlist) == -1 :
                 roomlist += [nextroom]
+        def GridFiller(r,c) :
+            validity = True
+            if not Rect(self.boundary_buffer, self.boundary_buffer, self.width - 2*self.boundary_buffer, self.height - 2*self.boundary_buffer).collidepoint(c,r) :
+                return DungeonData(LAB_WALL, False) 
+            for rm in roomlist :
+                if rm.collidepoint(c,r) :
+                    return DungeonData( LAB_FLOOR, False)
+                if validity :
+                    if Rect(rm.left-1,rm.top-1,rm.width+2,rm.height+2).collidepoint(c,r) :
+                        validity = False
+            return DungeonData( LAB_WALL, validity)
+        GM = GridMatrix(self.width, self.height, GridFiller)
+        self._build_corridors(GM)
+        rdict = {}
+        for node in GM.rowwise_iterator() :
+            if node.data.data == LAB_FLOOR :
+                rdict[ node.column, node.row] = LAB_FLOOR 
+        start_point, end_point = (0,0), (0,0)
+        #start_point = random.choice(list(rdict))
+        #end_point = random.choice(list(rdict))
+        
 
-        lab_dict = {}
-        room_dict_list = [self._fill_rect(r) for r in roomlist]
-        for d in room_dict_list :
-            lab_dict.update(d)
-
-        start_point = random.choice(list(lab_dict))
-        end_point = random.choice(list(lab_dict))
-
-        return Labyrinth(self.width, self.height, start_point, end_point, lab_dict)
+        return Labyrinth(self.width, self.height, start_point, end_point, rdict)
 
