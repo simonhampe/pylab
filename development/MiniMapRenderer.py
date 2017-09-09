acfa03c7c01df4514c0e121fe7b50efd77ffc388
Simon Hampe <simon.hampe@googlemail.com>
2017-09-09 15:04:19 +0200

    Startint refactoring of cellular generator

---
+++ development/MiniMapRenderer.py
@@ -0,0 +1,53 @@
+import pygame
+import sys
+from pygame import Color, draw
+
+from pygame.constants import QUIT
+from pygame.locals import Rect
+from gameData import LabyrinthConstants
+from generators.CellularGenerator import CellularGenerator
+
+
+class MinimapRenderer:
+    def __init__(self, labyrinth, field_size):
+        self.labyrinth = labyrinth
+        self.field_size = field_size
+        self.color_map = {LabyrinthConstants.LAB_FLOOR: Color(255, 178, 102),
+                          LabyrinthConstants.LAB_WALL: Color(50, 50, 50),
+                          LabyrinthConstants.LAB_END: Color(0,255,0),
+                          LabyrinthConstants.LAB_NIRVANA: Color(255,0,255)}
+
+    def draw(self, surface):
+        (width, height) = self.labyrinth.get_size()
+        surface.lock()
+        for col in range(0, width):
+            for row in range(0, height):
+                value = self.labyrinth.value_at(row, col)
+                color = self.color_map.get(value, Color(255, 255, 255))
+                draw.rect(surface, color,
+                          Rect(col * self.field_size, row * self.field_size, self.field_size, self.field_size))
+        surface.unlock()
+
+
+pygame.init()
+screen = pygame.display.set_mode((800, 800), pygame.RESIZABLE)
+screen.fill((255, 255, 255))
+pygame.display.set_caption('PlayerViewRenderer - Test')
+
+generator = CellularGenerator(200, 200)
+labyrinth = generator.generate_labyrinth()
+
+renderer = MinimapRenderer(labyrinth, 3)
+render_surface = screen.subsurface(Rect(20,20, 600,600))
+
+renderer.draw(render_surface)
+pygame.display.update()
+
+
+while True:
+
+    for event in pygame.event.get():
+        # exit event
+        if (event.type == QUIT) or ((event.type == pygame.KEYDOWN) and (event.key == pygame.K_ESCAPE)):
+            pygame.quit()
+            sys.exit()
--- generators/CellularGenerator.py
+++ generators/CellularGenerator.py
@@ -5,12 +5,53 @@ from gameData.Labyrinth import Labyrinth
 from gameData.LabyrinthConstants import *
 from tools import GridTools
 
-"""
- This creates a labyrinth from a single cellular automaton
-"""
+# TODO: Refactor for reuse with other parameters
+def iterate_cellular_generation(room, no_of_iterations=1, death_limit=3, birth_limit=5):
+    """
+    This takes a grid matrix and runs the given number of iteration of a cellular automaton
+    Elements will be marked either LAB_FLOOR or LAB_WALL at the end.
+    :param room: A grid matrix. The attached data will be
+    :param no_of_iterations:
+    :param death_limit:
+    :param birth_limit:
+    """
+    for it in range(0, no_of_iterations):
+        old_index = it % 2
+        new_index = (it + 1) % 2
+        for node in room.rowwise_iterator():
+            old_node_data = node.data[old_index]
+            nb_alive = len([0 for nb in node.neighbours() if nb.data[old_index] == LAB_FLOOR])
+            if old_node_data == LAB_FLOOR:
+                node.data[new_index] = LAB_FLOOR if nb_alive >= death_limit else LAB_WALL
+            else:
+                node.data[new_index] = LAB_FLOOR if nb_alive >= birth_limit else LAB_WALL
+    room.data_index = new_index
 
-class CellularGenerator :
 
+def find_component(room, node):
+    if node.data[room.data_index] != LAB_FLOOR:
+        return []
+    queue = [node]
+    component = []
+    while len(queue) > 0:
+        nd, queue = queue[0], queue[1:]
+        if len(nd.data) == 2:
+            nd.data += [0]
+            component += [nd]
+            queue += [x for x in nd.straight_neighbours() if x.data[room.data_index] == LAB_FLOOR]
+    return component
+
+
+def find_largest_component(room):
+    max_component = []
+    for node in room.rowwise_iterator():
+        n_cmp = find_component(room, node)
+        if len(n_cmp) > len(max_component):
+            max_component = n_cmp
+    return max_component
+
+
+class CellularGenerator:
     """
     Generates a labyrinth via a single cellular automaton running in several iterations
     """
@@ -18,59 +59,20 @@ class CellularGenerator :
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.chance_for_floor = 45
-        self.death_limit = 3
-        self.birth_limit = 5
-        self.iterations = 3
 
-    def _iterate(self, room, no_of_iterations = -1) :
-        if no_of_iterations < 0 :
-            no_of_iterations = self.iterations
-        for it in range(0, no_of_iterations) :
-            old_index = it % 2
-            new_index = (it+1) % 2
-            for node in room.rowwise_iterator() :
-                old_node_data = node.data[old_index]
-                nb_alive = len( [0 for nb in node.neighbours() if nb.data[old_index] == LAB_FLOOR])
-                if old_node_data == LAB_FLOOR :
-                    node.data[new_index] = LAB_FLOOR if nb_alive >= self.death_limit else LAB_WALL
-                else :
-                    node.data[new_index] = LAB_FLOOR if nb_alive >= self.birth_limit else LAB_WALL
-        room.data_index = new_index
-
-    def _component(self,room,node) :
-        if node.data[room.data_index] != LAB_FLOOR :
-            return []
-        queue = [node]
-        component = []
-        while len(queue) > 0 :
-            nd, queue = queue[0], queue[1:]
-            if(len(nd.data) == 2) :
-                nd.data += [0]
-                component += [nd]
-                queue += [x for x in nd.straight_neighbours() if x.data[room.data_index] == LAB_FLOOR]
-        return component
-
-    def _largest_component(self,room) :
-        max_component = []
-        for node in room.rowwise_iterator() :
-            n_cmp = self._component(room, node)
-            if len(n_cmp) > len(max_component) :
-                max_component = n_cmp
-        return max_component
-
-
-    def generate_labyrinth(self) :
-        room = GridMatrix(self.width, self.height, lambda r,c : [LAB_FLOOR, LAB_FLOOR] if random.randint(1,100) <= self.chance_for_floor else [LAB_WALL, LAB_WALL])
+    def generate_labyrinth(self):
+        chance_for_floor = 45
+        room = GridMatrix(self.width, self.height,
+                          lambda r, c: [LAB_FLOOR, LAB_FLOOR] if random.randint(1, 100) <= chance_for_floor else [
+                              LAB_WALL, LAB_WALL])
         room.data_index = 0
-        self._iterate(room)
-        comp = self._largest_component(room)
+        iterate_cellular_generation(room, 3)
+        comp = find_largest_component(room)
         rdict = {}
         for node in comp:
-            rdict[ (node.column, node.row) ] = LAB_FLOOR
+            rdict[(node.column, node.row)] = LAB_FLOOR
         start_point = random.choice(list(rdict))
-        all_end_points = sorted(rdict, key= lambda p : GridTools.manhattan_distance(start_point, p))
-        end_point = all_end_points[15] #random.choice( all_end_points[ -len(all_end_points)/3 :])
-        rdict[ end_point ] = LAB_END
+        all_end_points = sorted(rdict, key=lambda p: GridTools.manhattan_distance(start_point, p))
+        end_point = all_end_points[15]  # random.choice( all_end_points[ -len(all_end_points)/3 :])
+        rdict[end_point] = LAB_END
         return Labyrinth(self.width, self.height, start_point, rdict)
-
